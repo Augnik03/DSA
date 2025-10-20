0cf2d9693fbdd3dc975877a72ecdc9bc121cd771
Augnik Banerjee <74064653+Augnik03@users.noreply.github.com>
2025-10-20 06:11:41 +0530

    Time: 0 ms (100.00%) | Memory: 15.5 MB (88.14%) - LeetSync

--- Leetcode/LinkedList/328-odd-even-linked-list/odd-even-linked-list.cpp
+++ Leetcode/LinkedList/328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -17,9 +17,9 @@ public:
         even = head->next;
         evenHead = head->next;
         while(even && even->next){
-            odd->next = even->next;
+            odd->next = odd->next->next;
             odd = odd->next;
-            even->next = odd->next;
+            even->next = even->next->next;
             even = even->next;
         }
         odd->next = evenHead;
