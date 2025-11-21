4b5c75aec68e1272f94e1140367ae626f846bf7f
Augnik Banerjee <74064653+Augnik03@users.noreply.github.com>
2025-11-22 01:56:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (74.83%) - LeetSync

---
+++ Leetcode/Greedy/678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int low = 0, high = 0;
+        int n = s.length();
+        for(int i=0; i<n; i++){
+            if(s[i] == '('){
+                low++;
+                high++;
+            }else if(s[i] == ')'){
+                low--;
+                high--;
+            }else{
+                low--;
+                high++;
+            }
+            if(low<0) low = 0;
+            if(high<0) return false;
+        }
+        return (low == 0);
+    }
+};
